3eca01d4e38776656817a3c806d7c8cccd80e37d
ifWhileFor <mathis.grattepanche1@gmail.com>
2025-03-27 12:14:56 +0100

    wow

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // script.js
 function flipCard(cardElement) {
+  if (cardElement.classList.contains('flipped')) {
+    cardElement.classList.remove('flipped');
+    cardElement.classList.add('noflipped');
+
+  }
+  else {
     const cardInner = cardElement;
     cardInner.classList.toggle('flipped');
+    cardInner.classList.remove('noflipped');
+    
   }
+}
